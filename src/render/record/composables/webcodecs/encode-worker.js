ed942f10889afdfa87a12bc1f0f561936ea2c981
seemr <110106669+seemrcola@users.noreply.github.com>
2024-04-24 20:13:42 +0800

    wip: update electron@30 v30 support file-system-api

--- src/render/record/composables/webcodecs/encode-worker.js
+++ src/render/record/composables/webcodecs/encode-worker.js
@@ -1,14 +1,4 @@
-import  './webm-writer2.js'
-
-// self.ArrayBufferDataStream = ExtendArrayBufferDataStream()
-// self.BlobBuffer = ExtendBlobBuffer()
-// self.WebMWriter = ExtendWebMWriter()
-
-console.log(self.ArrayBufferDataStream)
-console.log(self.BlobBuffer)
-console.log(self.WebMWriter)
-
-console.log('WebMWriter loaded')
+import './webm-writer2.js'
 
 let webmWriter = null;
 let fileWritableStream = null;
@@ -31,8 +21,6 @@ async function startRecording(fileHandle, frameStream, trackSettings) {
     height: trackSettings.height
   });
 
-  console.log('WebMWriter created', webmWriter)
-
   // 根据frameStream创建一个ReadableStreamReader对象，用来读取帧数据
   frameReader = frameStream.getReader();
 
@@ -42,7 +30,6 @@ async function startRecording(fileHandle, frameStream, trackSettings) {
       webmWriter.addFrame(chunk);
     },
     error: (e) => {
-      console.log(e.message);
       stopRecording();
     }
   };
@@ -62,33 +49,34 @@ async function startRecording(fileHandle, frameStream, trackSettings) {
   encoder.configure(config);
 
   // 开始编码帧数据
-  frameReader.read().then(async function processFrame({ done, value }) {
-    let frame = value;
+  frameReader.read()
+    .then(async function processFrame({ done, value }) {
+      let frame = value;
 
-    if (done) {
-      await encoder.flush();
-      encoder.close();
-      return;
-    }
-
-    // 当编码器队列中的帧数小于30时，将帧数据编码并写入WebM文件
-    if (encoder.encodeQueueSize <= 30) {
-      // 当帧数是20的倍数时，打印日志
-      if (++frameCounter % 20 == 0) {
-        console.log(frameCounter + ' frames processed');
+      if (done) {
+        await encoder.flush();
+        encoder.close();
+        return;
       }
-      // 每隔150帧插入一个关键帧
-      const insert_keyframe = (frameCounter % 150) == 0;
-      encoder.encode(frame, { keyFrame: insert_keyframe });
-    } else {
-      console.log('dropping frame, encoder falling behind');
-    }
 
-    // 关闭帧数据 释放内存
-    frame.close();
-    // 读取下一帧数据
-    frameReader.read().then(processFrame);
-  });
+      // 当编码器队列中的帧数小于30时，将帧数据编码并写入WebM文件
+      if (encoder.encodeQueueSize <= 30) {
+        // 当帧数是20的倍数时，打印日志
+        if (++frameCounter % 20 == 0) {
+          console.log(frameCounter + ' frames processed');
+        }
+        // 每隔150帧插入一个关键帧
+        const insert_keyframe = (frameCounter % 150) == 0;
+        encoder.encode(frame, { keyFrame: insert_keyframe });
+      } else {
+        console.log('dropping frame, encoder falling behind');
+      }
+
+      // 关闭帧数据 释放内存
+      frame.close();
+      // 读取下一帧数据
+      frameReader.read().then(processFrame);
+    })
 }
 
 async function stopRecording() {
