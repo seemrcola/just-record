079447643f7a5b4cc758571dc37531dcc1a77a08
seemr <110106669+seemrcola@users.noreply.github.com>
2024-05-11 23:23:36 +0800

    fix: base-size

--- src/render/screenshot/components/BaseSize.vue
+++ src/render/screenshot/components/BaseSize.vue
@@ -1,23 +1,23 @@
 <script setup lang='ts'>
-import { ref } from 'vue'
 import type { Size } from '../types'
+
+const props = defineProps<{
+  size: Size
+}>()
 
 const emits = defineEmits<
   (e: 'changeSize', size: Size) => void
 >()
 
-const size = ref<Size>('small')
-
 function changeSize(newSize: Size) {
-  size.value = newSize
   emits('changeSize', newSize)
 }
 </script>
 
 <template>
   <div px-2 flex items-center text-light>
-    <div i-mdi:checkbox-blank-circle w-3 h-3 :class="{ 'bg-orange': size === 'small' }" @click="changeSize('small')" />
-    <div i-mdi:checkbox-blank-circle w-4 h-4 mx-2 :class="{ 'bg-orange': size === 'medium' }" @click="changeSize('medium')" />
-    <div i-mdi:checkbox-blank-circle w-5 h-5 :class="{ 'bg-orange': size === 'large' }" @click="changeSize('large')" />
+    <div i-mdi:checkbox-blank-circle w-3 h-3 :class="{ 'bg-orange': props.size === 'small' }" @click="changeSize('small')" />
+    <div i-mdi:checkbox-blank-circle w-4 h-4 mx-2 :class="{ 'bg-orange': props.size === 'medium' }" @click="changeSize('medium')" />
+    <div i-mdi:checkbox-blank-circle w-5 h-5 :class="{ 'bg-orange': props.size === 'large' }" @click="changeSize('large')" />
   </div>
 </template>
