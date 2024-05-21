b3e86942a63cdb8b11d4a6aa6de723b99c032798
seemr <110106669+seemrcola@users.noreply.github.com>
2024-05-22 00:17:32 +0800

    wip: isofix

--- src/main/useImage.ts
+++ src/main/useImage.ts
@@ -2,16 +2,16 @@ import { ipcMain, BrowserWindow } from 'electron'
 import { useImageWindow } from './windows/createImageWindow'
 
 const imgWinList: BrowserWindow[] = []
-
 export function useImage() {
   ipcMain.handle('isofix', async (event, opt: IsofixConfig) => {
     // 创建一个窗口
     const win = await useImageWindow(opt)
     // 将opt.base64数据传给win
+    // 新窗口有一个1000ms的loading时间
     setTimeout(() => {
       win.webContents.send('isofix-finished', { base64: opt.base64 })
       imgWinList.push(win)
-    }, 2000)
+    }, 1000)
   })
 
   ipcMain.handle('close', async () => {
